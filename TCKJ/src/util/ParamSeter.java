c53900e34ca40141533551d790c6bffc63dc6f16
dyb <892977595@qq.com>
2020-02-17 18:13:50 +0800

    删除tckj

--- TCKJ/src/util/ParamSeter.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-import java.sql.PreparedStatement;
-
-public interface ParamSeter {
-	public void setValue(PreparedStatement stmt);
-}
--- TCKJ/src/util/ResultSeter.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-import java.sql.ResultSet;
-//处理结果集
-public interface ResultSeter {
-	public void handler(ResultSet rs);
-}
